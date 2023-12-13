05d82ca00addeb828ee5bce78cf007c4704de9eb
Prabu <annamalaiprabudev@gmail.com>
2023-12-13 11:39:09 +0530

    Migrated from CRA to Vite

---
+++ jujubee-vite-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+
+import NavBar from './components/NavBar';
+
+const App = () => {
+
+  return (
+      <div>
+
+        <NavBar />
+
+        <h1> Jujubee Chat </h1>
+
+      </div>
+  );
+};
+
+export default App;
